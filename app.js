37c907ead3d1d6ca6d7fe941083a6f230ecfa160
= <=>
2018-02-12 10:52:38 +0000

    final

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var botbuilder_azure = require("botbuilder-azure");
 var Store = require('./store');
 var PropertiesReader = require('properties-reader');
 var properties = PropertiesReader('./properties.file');
+var https = require('https');
 
 
 // Setup Restify Server
@@ -36,43 +37,179 @@ var tableName = 'botdata';
 var azureTableClient = new botbuilder_azure.AzureTableClient(tableName, process.env['AzureWebJobsStorage']);
 var tableStorage = new botbuilder_azure.AzureBotStorage({ gzipData: false }, azureTableClient);
 
-// Create your bot with a function to receive messages from the user
+// Default response - with ChatBot info card
 var bot = new builder.UniversalBot(connector, function (session, args) {
-    session.send('Hi Welcome to the Accenture FAQ Bot :)');
-    session.send("How can I help you?");
+    session.send('Sorry,  I dont have an answer for that currently.<br><br>Can I help you with something else?');
+    var msg = new builder.Message(session).addAttachment(createHeroCard(session));
+    session.send(msg);
 });
+
+
+function createHeroCard(session) {
+        return new builder.HeroCard(session)
+            .title('Accenture FAQ Bot')
+            .subtitle('<<< Demo version >>>')
+            .text('The knowledge base is limited to the FAQ provided in the Technology Ireland Handbook')
+            .buttons([
+                builder.CardAction.openUrl(session, 'https://collections.accenture.com/FileViewer.aspx?f=f2ad46cc-344d-4de4-b2c3-cbad4a2562da', 'Read HandBook')
+            ]);;
+}
+        
 
 bot.set('storage', tableStorage);
 
-// Make sure you add code to validate these fields
-var luisAppId = process.env.LuisAppId;
-var luisAPIKey = process.env.LuisAPIKey;
-var luisAPIHostName = process.env.LuisAPIHostName || 'westus.api.cognitive.microsoft.com';
-
-const LuisModelUrl = 'https://' + luisAPIHostName + '/luis/v1/application?id=' + luisAppId + '&subscription-key=' + luisAPIKey;
-console.log("<><><>");
 // Main dialog with LUIS
 
 var recognizer = new builder.LuisRecognizer("https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/77ea1671-23d7-4bfb-a186-a2e9e53582cc?subscription-key=70255eaedfa0422f8b3f91a8aad987a7&verbose=true&timezoneOffset=0&q=");
 bot.recognizer(recognizer);
+
+// Handling each intends
 bot.dialog('rolefamily', function (session, args) {
     console.log("Role family !");
     var property = properties.get('rolefamily');
-    session.send(property);
+    session.endDialog(property);
 }).triggerAction({
     matches: 'rolefamily'
 });
 bot.dialog('changerolefamily', function (session, args) {
     console.log("Change Role family !");
     var property = properties.get('changerolefamily');
-    session.send(property);
+    session.endDialog(property);
 }).triggerAction({
     matches: 'changerolefamily'
 });
 bot.dialog('careercounsellor', function (session, args) {
     console.log("Counsellor!");
     var property = properties.get('careercounsellor');
-    session.send(property);
+    session.endDialog(property);
 }).triggerAction({
     matches: 'careercounsellor'
+});
+bot.dialog('greeting', function (session, args) {
+    console.log("greeting !");
+    
+    session.send('Hi Welcome to the Accenture FAQ Bot :)');
+    session.endDialog("How can I help you?");
+}).triggerAction({
+    matches: 'greeting'
+});
+
+// Enhanced
+
+bot.dialog('whereCanContractWork', function (session, args) {
+    console.log("whereCanContractWork!");
+    var property = properties.get('whereCanContractWork');
+    session.endDialog(property);
+}).triggerAction({
+    matches: 'whereCanContractWork'
+});
+bot.dialog('remainInRoleFamily', function (session, args) {
+    console.log("remainInRoleFamily!");
+    var property = properties.get('remainInRoleFamily');
+    session.endDialog(property);
+}).triggerAction({
+    matches: 'remainInRoleFamily'
+});
+bot.dialog('onlineResourceForRoleFamily', function (session, args) {
+    console.log("onlineResourceForRoleFamily!");
+    var property = properties.get('onlineResourceForRoleFamily');
+    session.endDialog(property);
+}).triggerAction({
+    matches: 'onlineResourceForRoleFamily'
+});
+bot.dialog('understandingRole', function (session, args) {
+    console.log("understandingRole!");
+    var property = properties.get('understandingRole');
+    session.endDialog(property);
+}).triggerAction({
+    matches: 'understandingRole'
+});
+bot.dialog('identifyPeopleOfSameRole', function (session, args) {
+    console.log("identifyPeopleOfSameRole!");
+    var property = properties.get('identifyPeopleOfSameRole');
+    session.endDialog(property);
+}).triggerAction({
+    matches: 'identifyPeopleOfSameRole'
+});
+bot.dialog('attendingTechnologyEvents', function (session, args) {
+    console.log("attendingTechnologyEvents!");
+    var property = properties.get('attendingTechnologyEvents');
+    session.endDialog(property);
+}).triggerAction({
+    matches: 'attendingTechnologyEvents'
+});
+bot.dialog('roleFamilyAndTalentDiscussions', function (session, args) {
+    console.log("roleFamilyAndTalentDiscussions!");
+    var property = properties.get('roleFamilyAndTalentDiscussions');
+    session.endDialog(property);
+}).triggerAction({
+    matches: 'roleFamilyAndTalentDiscussions'
+});
+bot.dialog('nonAlignedRoleAndPromotion', function (session, args) {
+    console.log("nonAlignedRoleAndPromotion!");
+    var property = properties.get('nonAlignedRoleAndPromotion');
+    session.endDialog(property);
+}).triggerAction({
+    matches: 'nonAlignedRoleAndPromotion'
+});
+bot.dialog('nonMatchingRole', function (session, args) {
+    console.log("nonMatchingRole!");
+    var property = properties.get('nonMatchingRole');
+    session.endDialog(property);
+}).triggerAction({
+    matches: 'nonMatchingRole'
+});
+bot.dialog('roleDictation', function (session, args) {
+    console.log("roleDictation!");
+    var property = properties.get('roleDictation');
+    session.endDialog(property);
+}).triggerAction({
+    matches: 'roleDictation'
+});
+bot.dialog('peopleAlignedToCD&O', function (session, args) {
+    console.log("peopleAlignedToCD&O!");
+    var property = properties.get('peopleAlignedToCD&O');
+    session.endDialog(property);
+}).triggerAction({
+    matches: 'peopleAlignedToCD&O'
+});
+bot.dialog('moveBtweenDTES', function (session, args) {
+    console.log("moveBtweenDTES!");
+    var property = properties.get('moveBtweenDTES');
+    session.endDialog(property);
+}).triggerAction({
+    matches: 'moveBtweenDTES'
+});
+bot.dialog('movingTOHigherLevels', function (session, args) {
+    console.log("movingTOHigherLevels!");
+    var property = properties.get('movingTOHigherLevels');
+    session.endDialog(property);
+}).triggerAction({
+    matches: 'movingTOHigherLevels'
+});
+bot.dialog('promotionConsideration', function (session, args) {
+    console.log("promotionConsideration!");
+    var property = properties.get('promotionConsideration');
+    session.endDialog(property);
+}).triggerAction({
+    matches: 'promotionConsideration'
+});
+bot.dialog('trainingForRole', function (session, args) {
+    console.log("trainingForRole!");
+    var property = properties.get('trainingForRole');
+    session.endDialog(property);
+}).triggerAction({
+    matches: 'trainingForRole'
+});
+
+bot.dialog('whatsTheTime', function (session, args, next) {
+    var country = builder.EntityRecognizer.findEntity(args.intent.entities, 'country');
+    
+    if (country) {
+      Store.searchHotels(country,session);
+    } else {
+           console.log("Time not fetch !");
+        }
+    }).triggerAction({
+    matches: 'whatsTheTime'
 });
--- store.js
+++ store.js
@@ -1,180 +1,50 @@
 var Promise = require('bluebird');
 
-var ReviewsOptions = [
-    '“Very stylish, great stay, great staff”',
-    '“good hotel awful meals”',
-    '“Need more attention to little things”',
-    '“Lovely small hotel ideally situated to explore the area.”',
-    '“Positive surprise”',
-    '“Beautiful suite and resort”'];
-
-
 // Code starts
 var key = "AIzaSyApmQaPNP838cZOeIGCibeq9cmAn7Tv8aQ";
-  var location = "40.7127,-74.0059";
-  var radius = 500;
-  var sensor = false;
-  var types = "restaurant";
-  var keyword = "fast";
 
-  var https = require('https');
-
-/*	var url1 = "https://maps.googleapis.com/maps/api/geocode/json?address=Dublin&key=AIzaSyC32xjSB9u0NsivbW6bA-qy6o7spmVko_U";
-			https.get(url1, function(response) {
-		    var body ='';
-		    response.on('data', function(chunk) {
-		      body += chunk;
-				
-			
-		    });
-				
-			response.on('end', function() {
-		      var places = JSON.parse(body);
-				console.log(places.results[0].geometry.location);
-				location = places.results[0].geometry.location.lat+","+places.results[0].geometry.location.lng;
-			});
-				
-			});
-
-*/
-
-
-  
-/*  var url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?" + "key=" + key + "&location=" + location + "&radius=" + radius + "&sensor=" + sensor + "&types=" + types + "&keyword=" + keyword;
-    console.log(url);
-  
-  https.get(url, function(response) {
-    var body ='';
-    response.on('data', function(chunk) {
-      body += chunk;
-    });
-  
-	 response.on('end', function() {
-      var places = JSON.parse(body);
-      var locations = places.results;
-      var randLoc = locations[Math.floor(Math.random() * locations.length)];
-	console.log(randLoc.geometry.location);
-		   console.log(">>>>>>>>>>>>>>>>>>>>>>");
-		   //console.log(locations[0].geometry.location);
-		 console.log(locations[0].name)
-     // res.json(randLoc);
-    });
-  });   */
-
-// Code ends
-
-
-
+var https = require('https');
 
 
 module.exports = {
-    searchHotels: function (destination) {
+    searchHotels: function (country,session) {
         return new Promise(function (resolve) {
-			
-			console.log(">>>>  Searching for hotels",destination);
-			var hotels =[];
-			var geocode="";
-			
-			// Geocoding from the destination
-			var url1 = "https://maps.googleapis.com/maps/api/geocode/json?address="+destination+"&key=AIzaSyC32xjSB9u0NsivbW6bA-qy6o7spmVko_U";
-			https.get(url1, function(response) {
-		    var body ='';
-		    response.on('data', function(chunk) {
-		      body += chunk;
-				
-			
-		    });
-				
-			response.on('end', function() {
-		      var places = JSON.parse(body);
-				console.log(places.results[0].geometry.location);
-				geocode = places.results[0].geometry.location.lat+","+places.results[0].geometry.location.lng;
-				
-				console.log(">>>",geocode);
-				var url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?" + "key=" + key + "&location=" + geocode + "&radius=" + radius + "&sensor=" + sensor + "&types=" + types + "&keyword=" + keyword;
-			
-			https.get(url, function(response) {
-		    var body ='';
-		    response.on('data', function(chunk) {
-		      body += chunk;
-		    });
-				
-			
-				
-			response.on('end', function() {
-				
-				
-		      var places = JSON.parse(body);
-		      var locations = places.results;
-		      var randLoc = locations[Math.floor(Math.random() * locations.length)];
-			console.log(randLoc.geometry.location);
-				   console.log(">>>>>>>>>>>>>>>>>>>>>>");
-				console.log(locations[0].name);
-				console.log(locations[1].name);
-				
-				for (var i = 0; i < locations.length; i++) {
-				   hotels.push({
-				   	name: locations[i].name,
-					image: locations[i].icon,
-					location: destination,
-                    rating: Math.ceil(Math.random() * 5),
-                    numberOfReviews: Math.floor(Math.random() * 5000) + 1,
-                    priceStarting: Math.floor(Math.random() * 450) + 80
-				   })
-				}
-				hotels.sort(function (a, b) { return a.priceStarting - b.priceStarting; });
-
-	            // complete promise with a timer to simulate async response
-	            setTimeout(function () { resolve(hotels); }, 1000);
-				
-				
-		     // res.json(randLoc);
-		    });
-				
-				
-			});
-				
-			});
-				
-				
-				
-			});
-				
-			// Fetching the hotels in the destination
-			
-
-            // Filling the hotels results manually just for demo purposes
-           /* var hotels = [];
-            for (var i = 1; i <= 5; i++) {
-                hotels.push({
-                    name: destination + ' Hotel ' + i,
-                    location: destination,
-                    rating: Math.ceil(Math.random() * 5),
-                    numberOfReviews: Math.floor(Math.random() * 5000) + 1,
-                    priceStarting: Math.floor(Math.random() * 450) + 80,
-                    image: 'https://placeholdit.imgix.net/~text?txtsize=35&txt=Hotel+' + i + '&w=500&h=260'
-                });
-            } */
-
             
-        });
-    },
-
-    searchHotelReviews: function (hotelName) {
-        return new Promise(function (resolve) {
-
-            // Filling the review results manually just for demo purposes
-            var reviews = [];
-            for (var i = 0; i < 5; i++) {
-                reviews.push({
-                    title: ReviewsOptions[Math.floor(Math.random() * ReviewsOptions.length)],
-                    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris odio magna, sodales vel ligula sit amet, vulputate vehicula velit. Nulla quis consectetur neque, sed commodo metus.',
-                    image: 'https://upload.wikimedia.org/wikipedia/en/e/ee/Unknown-person.gif'
+             var resp = "";
+              //---- Start - Consuming google api for geocoding & timezonedb for getting time.
+        console.log(">>>>>> Country: ",country)
+        var url1 = "https://maps.googleapis.com/maps/api/geocode/json?address="+country.entity+"&key=AIzaSyC32xjSB9u0NsivbW6bA-qy6o7spmVko_U";
+        console.log("URL>>:",url1);
+        https.get(url1, function(response) {
+    		console.log("<<<Body "+resp);
+    		response.on('data', function(data) {
+                resp += data;
+    				
+    			
+    		});
+				
+			response.on('end', function() {
+                var places = JSON.parse(resp);
+				console.log(places.results[0].geometry.location);
+				var lat = places.results[0].geometry.location.lat;
+                var lng = places.results[0].geometry.location.lng;
+				console.log(">> Loc >>",places.results[0].geometry.location);
+				var url = "https://api.timezonedb.com/v2/get-time-zone?key=E3MNOE29SUJD&format=json&by=position&lat="+lat+"&lng="+lng;
+			
+        		https.get(url, function(response) {
+                    var body ='';
+                    response.on('data', function(chunk) {
+            		      body += chunk;
+            		});
+                    response.on('end', function() {
+                        var timeZone = JSON.parse(body);
+                        var time = timeZone.formatted;
+                        console.log(">>>>>>>> timezone>",time);
+                         session.endDialog("Current time in "+country.entity+" is "+time);
+                    });
                 });
-            }
-
-            // complete promise with a timer to simulate async response
-            setTimeout(function () { resolve(reviews); }, 1000);
+            });
+        });
         });
     }
 };
